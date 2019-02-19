42ec6f343e27474769f58bcdc71c170792facbac
Kirill <xakdog@gmail.com>
2019-02-19 23:09:31 +0300

    Frontend integraion with Djagno. Refactoring.

--- frontend/components/stage-win.tag
+++
@@ -1,9 +0,0 @@
-<stage-win>
-    <div class="game-area">
-        <h2 class="game-area__h2">You are win!</h2>
-
-        <p class="game-area__rules">
-            <fancy-button onclick={ opts.onagainpressed }>Play again?</fancy-button>
-        </p>
-    </div>
-</stage-win>
--- hangman/urls.py
+++ hangman/urls.py
@@ -19,7 +19,16 @@ from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 
+from page import views
+
 urlpatterns = [
+    path('', views.main),
     path('api/', include('api.urls', namespace='api')),
     path('admin/', admin.site.urls),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.STATIC_URL,
+        document_root=settings.STATIC_ROOT,
+    )
